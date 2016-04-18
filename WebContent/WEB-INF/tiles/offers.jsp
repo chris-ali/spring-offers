b921e706f8544375e5028b19522a383d1368c395
Christopher Ali <christopher.ali@outlook.com>
2016-04-18 15:34:30 -0400

    Added Log4J Logging
    - Added Log4J logging along with logger object in HomeController;
    currently set to display only INFO tagged messages
    
    - Resolved jcl-over-slf4j jar dependency conflict with Tiles

--- WebContent/WEB-INF/tiles/offers.jsp
+++ WebContent/WEB-INF/tiles/offers.jsp
@@ -1,7 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-
 <h2>Current Offers</h2>
 <table class="offers">
 	<tr><td>Name</td><td>Email</td><td>Offer</td></tr>
--- src/com/chrisali/spring/web/controllers/HomeController.java
+++ src/com/chrisali/spring/web/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package com.chrisali.spring.web.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	
+	private static Logger logger = Logger.getLogger(HomeController.class);
 
 	@RequestMapping("/")
 	public String showHome() {
+		logger.info("To home page...");
 		return "home";
 	}
 	
